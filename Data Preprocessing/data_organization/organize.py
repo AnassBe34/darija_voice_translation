0079119c4f8b80381931098117f4d7449cb4c212
AnassBE <138825492+AnassBe34@users.noreply.github.com>
2024-12-24 16:18:19 +0100

    Update organize.py

--- Data Preprocessing/data_organization/organize.py
+++ Data Preprocessing/data_organization/organize.py
@@ -1,9 +1,11 @@
 import os
 from pydub import AudioSegment
 
+#YOU CAN ADJUST THIS FILE BASED ON THE CHUNKS PATHS
+
 f = open('test.txt', 'w', encoding="utf-8")
-f.write('path|transcript\n')
-for chunk_index in range(1500, 1900) :
+f.write('path|transcript\n') 
+for chunk_index in range(1500, 1900) : ## CHANGE IT TO CHUNKS YOU WANT TO TRAIN THE MODEL ON
     audio_path = fr"C:\Users\ASUS\Desktop\dataset\dataset_0\audio_chunk_{chunk_index}.wav"
     transcription_path = fr"C:\Users\ASUS\Desktop\dataset\dataset_0\audio_chunk_{chunk_index}.txt"
     if os.path.exists(audio_path) :
